8ce5c382f9863f521042bed2477341fe87c1f2d8
YoussefCheikhani <youssef.cheikhani@gmail.com>
2021-02-09 18:01:56 +0100

    last commit

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import reducers from './src/reducers';
+import Routes from './src/Routes';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+        <Routes />
+      </Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,29 @@
 import { StackNavigator } from 'react-navigation';
 
-const Routes = StackNavigator({
+import Splash from './components/Splash';
+import Login from './components/Login';
+import Chat from './components/Chat';
 
+const Routes = StackNavigator({
+    Splash: {
+        screen: Splash,
+        navigationOptions: {
+            header: false
+        }
+    },
+    Login: {
+        screen: Login,
+        navigationOptions: {
+            header: false
+        }
+    },
+    Chat: {
+        screen: Chat,
+        navigationOptions: {
+            header: false
+        }
+    }
 });
+
 
 export default Routes;
