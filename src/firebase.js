fda09fd7efa5438fb251083fd0b9136c88692f43
YoussefCheikhani <youssef.cheikhani@gmail.com>
2022-12-10 11:23:50 +0100

    updated credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import * as firebase from 'firebase';
+import '../credentials';
+
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBKsqpF9QPCEMxr2TVGl8t5dy7Wt5cENBs",
-    authDomain: "messengerapp-79c42.firebaseapp.com",
-    databaseURL: "https://messengerapp-79c42-default-rtdb.firebaseio.com",
-    projectId: "messengerapp-79c42",
-    storageBucket: "messengerapp-79c42.appspot.com",
-    messagingSenderId: "713650007579",
-    appId: "1:713650007579:web:0ace068daf2978bb4212be",
-    measurementId: "G-ZYGQ153HN1"
+    apiKey: ApiKey,
+    authDomain: AuthDomain,
+    databaseURL: DatabaseURL,
+    projectId: ProjectId,
+    storageBucket: StorageBucket,
+    messagingSenderId: MessagingSenderId,
+    appId: AppId,
+    measurementId: MeasurementId
   };
 
   firebase.initializeApp(config);
